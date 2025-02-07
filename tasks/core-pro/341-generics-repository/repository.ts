efbee1eafaafd5a97a0df1d2ef7a3c30ebd54655
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-02-07 15:16:31 +0000

    412 task

--- tasks/core-pro/341-generics-repository/repository.ts
+++ tasks/core-pro/341-generics-repository/repository.ts
@@ -6,6 +6,7 @@ export interface User {
   email: string;
 }
 
+
 export class UserRepository {
   private dataAccess: DataAccess;
 
--- tasks/core-pro/412-mapped-extender/task.ts
+++ tasks/core-pro/412-mapped-extender/task.ts
@@ -1,9 +1,18 @@
+// Zaimplementuj typ mapowany type FieldExtender<T, K>, który dla podanego typu T tworzy nowy typ rozszerzony - 
+// lista pól jest taka sama, ale zamiast typów prostych każde pole staje się typem złożonym z dodatkowym polem “value”. 
+// Dostępne testy i kod przedstawiają docelowy obiekt, 
+// którego kompilacja powinna zostać przez ciebie odblokowana
+
 type Person = {
   firstName: string;
   lastName: string;
 };
 
-type FieldExtender<T, K> = Person;
+type FieldExtender<T, K> = {
+  [P in keyof T]: {
+    value: T[P];
+  } & K;
+};
 
 type PersonUpdateHistory = FieldExtender<
   Person,
@@ -25,3 +34,5 @@ export const history: PersonUpdateHistory = {
     updatedAt: new Date().getTime(),
   },
 };
+
+
