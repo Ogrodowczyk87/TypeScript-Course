4bc14c843e0c37eb47220d96a69ef438a7d57b00
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-02-07 15:21:39 +0000

    update

--- tasks/core-pro/321-generics-identity/identity.ts
+++ tasks/core-pro/321-generics-identity/identity.ts
@@ -55,4 +55,28 @@ class RedditIdentityProcessor {
   }
 }
 
-export class IdentityProcessor {}
+export class IdentityProcessor {
+  constructor(private provider: 'google' | 'apple' | 'reddit') {}
+
+  findById(id: string): GoogleIdentity | AppleIdentity | RedditIdentity | undefined {
+    switch (this.provider) {
+      case 'google':
+        return new GoogleIdentityProcessor().findById(id);
+      case 'apple':
+        return new AppleIdentityProcessor().findById(id);
+      case 'reddit':
+        return new RedditIdentityProcessor().findById(id);
+    }
+  }
+
+  findByUserName(userName: string): GoogleIdentity | AppleIdentity | RedditIdentity | undefined {
+    switch (this.provider) {
+      case 'google':
+        return new GoogleIdentityProcessor().findByUserName(userName);
+      case 'apple':
+        return new AppleIdentityProcessor().findByUserName(userName);
+      case 'reddit':
+        return new RedditIdentityProcessor().findByUserName(userName);
+    }
+  }
+}
