6bb6a558292132b1dc3820db9e50da4a3e85a45c
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-02-20 09:27:02 +0000

    421 pass

--- tasks/core-pro/421-literals-code/code-fail.ts
+++ tasks/core-pro/421-literals-code/code-fail.ts
@@ -1,5 +1,6 @@
 import { Code } from './task.ts';
 
+
 const code4: Code = '111000111';
 const code5: Code = '000';
 const code6: Code = '1';
--- tasks/core-pro/421-literals-code/task.ts
+++ tasks/core-pro/421-literals-code/task.ts
@@ -1,5 +1,9 @@
-export type Code = null;
 
-export function codeToDecimal(code: Code) {
-  throw new Error('Not implemented');
+
+export type Code = '110-111-001' | '000-000-101' | '011-100-101';
+
+export function codeToDecimal(code: Code): string {
+  const parts = code.split('-');
+  const decimalParts = parts.map(part => parseInt(part, 2).toString());
+  return decimalParts.join('');
 }
