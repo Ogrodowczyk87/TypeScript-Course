04b3867e1467b7b247c9bedb839a9f7c8101cea6
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-03-01 17:18:35 +0000

    442 2/3 pass

--- tasks/core-pro/442-infer-flags/task.ts
+++ tasks/core-pro/442-infer-flags/task.ts
@@ -1,11 +1,24 @@
 import { type FeatureFlags } from './legacy-flags.ts';
 
-type FlagsV2<T> = {};
+type FlagsV2<T> = {
+  [K in keyof T as K extends `${string}V2` ? K : never]: T[K]; 
+}
 
 export type ModernFeatureFlags = FlagsV2<FeatureFlags>;
 
 export function getFeatureFlagsV2(flags: FeatureFlags): ModernFeatureFlags {
-  const flagsV2: ModernFeatureFlags = {};
+  const flagsV2 = {
+    isSearchEnabled: false,
+    isContactFormEnabled: false,
+    isCartEnabled: false,
+  } as ModernFeatureFlags;
+
+  for (const key in flags) {
+    if (key.endsWith('V2')) {
+      const modernKey = key.substring(0, key.length - 2);
+      (flagsV2 as any)[modernKey] = flags[key as keyof FeatureFlags];
+    }
+  }
 
   return flagsV2;
 }
