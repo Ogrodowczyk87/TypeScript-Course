a40d1219b1674fd04b5f90d8465e2fda1cf96a1a
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-04-03 18:16:12 +0100

    631 pass

--- tasks/core-pro/631-extended-local-window/task.ts
+++ tasks/core-pro/631-extended-local-window/task.ts
@@ -2,6 +2,15 @@
   Zrób to w sposób lokalny, który będzie dostępny tylko w tym module.
 */
 
+
+interface ExtendedWindow extends Window {
+  storage: {
+    temporaryValue: string;
+  };
+}
+
+declare const window: ExtendedWindow;
+
 window.storage = {
   temporaryValue: 'Am I testing this code or is it testing me?',
 };
