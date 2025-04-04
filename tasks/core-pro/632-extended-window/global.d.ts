dca8648b06c6807ffa9ba9fb4ca54b4a63b9cc26
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2025-04-04 14:13:04 +0100

    632 pass

---
+++ tasks/core-pro/632-extended-window/global.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  analytics: {
+    trackEvent(eventName: string): void;
+  };
+}
--- tasks/core-pro/632-extended-window/tracking-code.ts
+++ tasks/core-pro/632-extended-window/tracking-code.ts
@@ -1,1 +1,12 @@
+if(!window.analytics) {
+    window.analytics = {
+        trackEvent(eventName: string) {
+            console.log(`Event tracked: ${eventName}`)
+        }
+    }
+}
+
+
+
 window.analytics.trackEvent('submit');
+
